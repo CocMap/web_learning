2f8cd7f53a0f405ec5c322093ae6efe27442f90a
Coc <ntranf108@gmail.com>
2017-10-03 22:52:06 +0700

    update reset number of items after TRASH_CLICK

--- tutorial3/jquery/js/myScript.js
+++ tutorial3/jquery/js/myScript.js
@@ -8,5 +8,6 @@ function ADD_TOTAL_ITEM(){
 
 function TRASH_CLICK(){
     document.getElementById("num_of_item").innerHTML = 0;       //set back to ZERO
+    total_item_click = 0;                                       //get num_of_item back to ZERO 
 }
 
